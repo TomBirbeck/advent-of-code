85ce88961b702f6d6c73c70718a8230973a081e7
unknown <tombirbeck47@gmail.com>
2022-12-04 01:05:36 +0000

    part 2 complete

--- day3.js
+++ day3.js
@@ -4,7 +4,6 @@ const items = readFileSync('day3.txt', { encoding: 'utf-8' })
   .replace(/\r/g, '')
   .trim()
   .split('\n');
-// console.log(items);
 
 const tests = [
   'vJrwpWtwJgWrhcsFMMfFFhFp',
@@ -20,15 +19,6 @@ function Backpack(items) {
   for (let i = 0; i < items.length; i++) {
     const arr1 = items[i].slice(0, items[i].length / 2).split('');
     const arr2 = items[i].slice(items[i].length / 2).split('');
-    // console.log(
-    //   items[i].length,
-    //   'arr1',
-    //   arr1,
-    //   arr1.length,
-    //   'arr2',
-    //   arr2,
-    //   arr2.length
-    // );
     for (let j = 0; j < arr1.length; j++) {
       if (arr2.includes(arr1[j])) {
         singles.push(arr1[j]);
@@ -38,17 +28,38 @@ function Backpack(items) {
   }
   return singles
 }
+function ThreeBackpack(items) {
+  const singles = [];
+  for (let i = 0; i <= items.length; i+=3) {
+    const arr1 = items[i];
+    const arr2 = items[i+1];
+    const arr3 = items[i+2];
+    if (arr1 && arr2 && arr3){
+      for (let j = 0; j < arr1.length; j++) {
+       if (arr2.includes(arr1[j]) && arr3.includes(arr1[j])) {
+          singles.push(arr1[j]);
+          j=arr1.length
+        }
+      }
+    }
+    }
+  return singles
+}
+
+// ThreeBackpack(tests)
 
 function Score(){
   let total = 0
   const priorities = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-  const arr = Backpack(items)
+  const arr = ThreeBackpack(items)
   for (let i = 0; i < arr.length; i++){
     const prio = (element) => element === arr[i]
-    total += (priorities.findIndex(prio)) + 1
+    total += ((priorities.findIndex(prio)) + 1)
   }
   console.log(total)
 }
 
 Score()
 
+//part 1 7766 ?
+//part 2 2415
