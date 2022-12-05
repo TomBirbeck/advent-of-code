b646e7c234338b9f9bf927e39cd9ac28270e4741
unknown <tombirbeck47@gmail.com>
2022-12-05 00:06:41 +0000

    fixed error I found with 1st part

--- day4.js
+++ day4.js
@@ -7,19 +7,17 @@ const pairs = readFileSync('day4.txt', { encoding: 'utf-8' })
 
 //   console.log(pairs)
 
-  function ranges(arr){
+function ranges(arr){
     let count = 0
     for (let i = 0; i < arr.length; i++){
         const split = arr[i].split(',')
-        const elfOne = split[0].split('-')
-        const elfTwo = split[1].split('-')
-        if (elfOne[0] >= elfTwo[0] && elfOne[1] <= elfTwo[1]){
-            console.log("1", elfOne, "2", elfTwo, "count", count)
+        const elfOne = split[0].split('-').map(Number)
+        const elfTwo = split[1].split('-').map(Number)
+        if (elfOne[0] >= elfTwo[0] && elfOne[0] <= elfTwo[1] && elfOne[1] <= elfTwo[1]){
             count++
-        }
-        else if (elfTwo[0] >= elfOne[0] && elfTwo[1] >= elfOne[1]){
-            console.log("1", elfOne, "2", elfTwo, "countTwo", count)
-            count++
+        } else if (elfTwo[0] >= elfOne[0] && elfTwo[0] <= elfOne[1] && elfTwo[1] <= elfOne[1]){
+          // console.log("1", elfOne, "2", elfTwo, "count", count)
+          count ++
         }
     }
     console.log(count)
@@ -28,3 +26,20 @@ const pairs = readFileSync('day4.txt', { encoding: 'utf-8' })
   ranges(pairs)
 
   //part 1 580
+
+  function rangesTwo(arr){
+    let count = 0
+    for (let i = 0; i < arr.length; i++){
+        const split = arr[i].split(',')
+        const elfOne = split[0].split('-')
+        const elfTwo = split[1].split('-')
+        if (elfOne[0] <= elfTwo[1] && elfTwo[0] <= elfOne[1]){
+            console.log("1", elfOne, "2", elfTwo, "count", count)
+            count++
+         
+    }
+    console.log(count)
+  }
+}
+
+//   rangesTwo(pairs)
